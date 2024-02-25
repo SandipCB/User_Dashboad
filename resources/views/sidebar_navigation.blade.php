710de74dc0e05b76d0ba083b84a03f924b33bb8e
SandipCB <chakrabortysandip872@gmail.com>
2024-02-26 04:43:13 +0600

    first commit

---
+++ resources/views/sidebar_navigation.blade.php
@@ -0,0 +1,38 @@
+ <!-- Sidebar Navigation Bar Start-->
+ <div class="d-flex align-items-stretch">
+     <nav id="sidebar">
+         <!-- Sidebar Navigation Header Start-->
+         <div class="sidebar-header d-flex align-items-center">
+             <div class="avatar"><img src="usercss/img/avatar-6.jpg" alt="..." class="img-fluid rounded-circle"></div>
+             <div class="title">
+                 <h1 class="h5">Sandip CB</h1>
+                 <p>UX/UI Designer,<br> Web Developer</p>
+             </div>
+         </div>
+         <!-- Sidebar Navigation Header End-->
+
+         <!-- Sidebar Navigation Menus Start-->
+         <span class="heading">Main</span>
+         <ul class="list-unstyled">
+             <li class="active"><a href="index.html"> <i class="icon-home"></i>Home Dashboard </a></li>
+             <li><a href="jobpreparation.html"> <i class="icon-padnote"></i>Job Preparation Panel </a></li>
+             <li><a href="forms.html"> <i class="icon-padnote"></i>Forms Checkup</a></li>
+             <li><a href="tables.html"> <i class="icon-grid"></i>Data Tables </a></li>
+             <li><a href="charts.html"> <i class="fa fa-bar-chart"></i>user Charts </a></li>
+             <!-- <li><a href="forms.html"> <i class="icon-padnote"></i>Forms</a></li> -->
+             <li><a href="#exampledropdownDropdown" aria-expanded="false" data-toggle="collapse"> <i class="icon-windows"></i>Example dropdown </a>
+                 <ul id="exampledropdownDropdown" class="collapse list-unstyled ">
+                     <li><a href="#">Page - 01</a></li>
+                     <li><a href="#">Page - 02</a></li>
+                     <li><a href="#">Page - 03</a></li>
+                 </ul>
+             </li>
+             <!-- <li><a href="login.html"> <i class="icon-logout"></i>Login page </a></li> -->
+         </ul><span class="heading">Extras</span>
+         <ul class="list-unstyled">
+             <li> <a href="#"> <i class="icon-settings"></i>Demo - 01 </a></li>
+             <li> <a href="#"> <i class="icon-writing-whiteboard"></i>Demo - 02 </a></li>
+             <li> <a href="#"> <i class="icon-chart"></i>Demo - 03</a></li>
+         </ul>
+     </nav>
+     <!-- Sidebar Navigation Bar end-->
---
+++ resources/views/user_header.blade.php
@@ -0,0 +1,209 @@
+<header class="header">
+    <nav class="navbar navbar-expand-lg">
+      <!-- Search and Close Search Close -->
+      <div class="search-panel">
+        <div class="search-inner d-flex align-items-center justify-content-center">
+          <div class="close-btn">Close <i class="fa fa-close"></i></div>
+          <form id="searchForm" action="#">
+            <div class="form-group">
+              <input type="search" name="search" placeholder="What are you Looking for...">
+              <button type="submit" class="submit">Search</button>
+            </div>
+          </form>
+        </div>
+      </div>
+      <!-- Search and Close Search End -->
+
+      <!-- Navbar Header-->
+
+      <div class="container-fluid d-flex align-items-center justify-content-between">
+        <div class="navbar-header">
+          <a href="index.html" class="navbar-brand">
+            <div class="brand-text brand-big visible text-uppercase"><strong class="text-primary">Campus</strong><strong>360</strong>
+              <div class="brand-note" style="font-size: 10px; color: #999; font-weight: 300; margin-top: 5px;">By Campus
+                Dot Crew </div>
+            </div>
+            <div class="brand-text brand-sm"><strong class="text-primary">Campus</strong><strong>360</strong></div>
+          </a>
+          <!-- Sidebar Toggle Btn-->
+          <button class="sidebar-toggle"><i class="fa fa-long-arrow-left"></i></button>
+        </div>
+        <!-- SMS Menu Start  -->
+        <div class="right-menu list-inline no-margin-bottom">
+          <div class="list-inline-item"><a href="#" class="search-open nav-link"><i class="icon-magnifying-glass-browser"></i></a></div>
+          <div class="list-inline-item dropdown"><a id="navbarDropdownMenuLink1" href="http://example.com" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" class="nav-link messages-toggle"><i class="icon-email"></i><span class="badge dashbg-1">5</span></a>
+            <div aria-labelledby="navbarDropdownMenuLink1" class="dropdown-menu messages"><a href="#" class="dropdown-item message d-flex align-items-center">
+                <div class="profile"><img src="usercss/img/avatar-3.jpg" alt="..." class="img-fluid">
+                  <div class="status online"></div>
+                </div>
+                <div class="content"> <strong class="d-block">Sandip Chakraborty</strong><span class="d-block">Can you
+                    cross check the new blog post?</span><small class="date d-block">9:30am</small></div>
+              </a><a href="#" class="dropdown-item message d-flex align-items-center">
+                <div class="profile"><img src="usercss/img/avatar-2.jpg" alt="..." class="img-fluid">
+                  <div class="status away"></div>
+                </div>
+                <div class="content"> <strong class="d-block">Peter Ramsy</strong><span class="d-block">Please accept my
+                    blog.</span><small class="date d-block">7:40am</small></div>
+              </a><a href="#" class="dropdown-item message d-flex align-items-center">
+                <div class="profile"><img src="usercss/img/avatar-1.jpg" alt="..." class="img-fluid">
+                  <div class="status busy"></div>
+                </div>
+                <div class="content"> <strong class="d-block">Sam CB</strong><span class="d-block">I'm facing some
+                    course related problem. Any user can help me?</span><small class="date d-block">6:55am</small>
+                </div>
+              </a><a href="#" class="dropdown-item message d-flex align-items-center">
+                <div class="profile"><img src="usercss/img/avatar-5.jpg" alt="..." class="img-fluid">
+                  <div class="status offline"></div>
+                </div>
+                <div class="content"> <strong class="d-block">Mahdi Hasan</strong><span class="d-block">Need some
+                    modification. Please work on it.</span><small class="date d-block">10:30pm</small>
+                </div>
+              </a><a href="#" class="dropdown-item text-center message"> <strong>See All Messages <i class="fa fa-angle-right"></i></strong></a>
+            </div>
+          </div>
+          <!-- SMS menu end  -->
+          <!-- Tasks Bar Start-->
+          <div class="list-inline-item dropdown"><a id="navbarDropdownMenuLink2" href="http://example.com" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" class="nav-link tasks-toggle"><i class="icon-new-file"></i><span class="badge dashbg-3">9</span></a>
+            <div aria-labelledby="navbarDropdownMenuLink2" class="dropdown-menu tasks-list"><a href="#" class="dropdown-item">
+                <div class="text d-flex justify-content-between"><strong>Task 1</strong><span>40% complete</span></div>
+                <div class="progress">
+                  <div role="progressbar" style="width: 40%" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" class="progress-bar dashbg-1"></div>
+                </div>
+              </a><a href="#" class="dropdown-item">
+                <div class="text d-flex justify-content-between"><strong>Task 2</strong><span>20% complete</span></div>
+                <div class="progress">
+                  <div role="progressbar" style="width: 20%" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100" class="progress-bar dashbg-3"></div>
+                </div>
+              </a><a href="#" class="dropdown-item">
+                <div class="text d-flex justify-content-between"><strong>Task 3</strong><span>70% complete</span></div>
+                <div class="progress">
+                  <div role="progressbar" style="width: 70%" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100" class="progress-bar dashbg-2"></div>
+                </div>
+              </a><a href="#" class="dropdown-item">
+                <div class="text d-flex justify-content-between"><strong>Task 4</strong><span>30% complete</span></div>
+                <div class="progress">
+                  <div role="progressbar" style="width: 30%" aria-valuenow="30" aria-valuemin="0" aria-valuemax="100" class="progress-bar dashbg-4"></div>
+                </div>
+              </a><a href="#" class="dropdown-item">
+                <div class="text d-flex justify-content-between"><strong>Task 5</strong><span>65% complete</span></div>
+                <div class="progress">
+                  <div role="progressbar" style="width: 65%" aria-valuenow="65" aria-valuemin="0" aria-valuemax="100" class="progress-bar dashbg-1"></div>
+                </div>
+              </a><a href="#" class="dropdown-item text-center"> <strong>See All Tasks <i class="fa fa-angle-right"></i></strong></a>
+            </div>
+          </div>
+          <!-- Tasks Bar end-->
+
+          <!-- Marge menu Start-->
+          <div class="list-inline-item dropdown menu-large"><a href="#" data-toggle="dropdown" class="nav-link">Mega <i class="fa fa-ellipsis-v"></i></a>
+            <div class="dropdown-menu megamenu">
+              <div class="row">
+                <div class="col-lg-3 col-md-6"><strong class="text-uppercase">Elements Heading - 1</strong>
+                  <ul class="list-unstyled mb-3">
+                    <li><a href="#">New Element Will Check - 01</a></li>
+                    <li><a href="#">New Element Will Check - 02</a></li>
+                    <li><a href="#">New Element Will Check - 03</a></li>
+                    <li><a href="#">New Element Will Check - 04</a></li>
+                    <li><a href="#">New Element Will Check - 05</a></li>
+                    <li><a href="#">New Element Will Check - 05</a></li>
+                    <li><a href="#">New Element Will Check - 06</a></li>
+                    <li><a href="#">New Element Will Check - 07</a></li>
+                  </ul>
+                </div>
+                <div class="col-lg-3 col-md-6"><strong class="text-uppercase">Elements Heading - 2</strong>
+                  <ul class="list-unstyled mb-3">
+                    <li><a href="#">New Element Will Check - 01</a></li>
+                    <li><a href="#">New Element Will Check - 02</a></li>
+                    <li><a href="#">New Element Will Check - 03</a></li>
+                    <li><a href="#">New Element Will Check - 04</a></li>
+                    <li><a href="#">New Element Will Check - 05</a></li>
+                    <li><a href="#">New Element Will Check - 05</a></li>
+                    <li><a href="#">New Element Will Check - 06</a></li>
+                    <li><a href="#">New Element Will Check - 07</a></li>
+                  </ul>
+                </div>
+                <div class="col-lg-3 col-md-6"><strong class="text-uppercase">Elements Heading - 3</strong>
+                  <ul class="list-unstyled mb-3">
+                    <li><a href="#">New Element Will Check - 01</a></li>
+                    <li><a href="#">New Element Will Check - 02</a></li>
+                    <li><a href="#">New Element Will Check - 03</a></li>
+                    <li><a href="#">New Element Will Check - 04</a></li>
+                    <li><a href="#">New Element Will Check - 05</a></li>
+                    <li><a href="#">New Element Will Check - 05</a></li>
+                    <li><a href="#">New Element Will Check - 06</a></li>
+                    <li><a href="#">New Element Will Check - 07</a></li>
+                  </ul>
+                </div>
+                <div class="col-lg-3 col-md-6"><strong class="text-uppercase">Elements Heading - 4</strong>
+                  <ul class="list-unstyled mb-3">
+                    <li><a href="#">New Element Will Check - 01</a></li>
+                    <li><a href="#">New Element Will Check - 02</a></li>
+                    <li><a href="#">New Element Will Check - 03</a></li>
+                    <li><a href="#">New Element Will Check - 04</a></li>
+                    <li><a href="#">New Element Will Check - 05</a></li>
+                    <li><a href="#">New Element Will Check - 05</a></li>
+                    <li><a href="#">New Element Will Check - 06</a></li>
+                    <li><a href="#">New Element Will Check - 07</a></li>
+                  </ul>
+                </div>
+              </div>
+
+              <div class="row megamenu-buttons text-center">
+                <div class="col-lg-2 col-md-4"><a href="#" class="d-block megamenu-button-link dashbg-1"><i class="fa fa-clock-o"></i><strong>Demo 1</strong></a></div>
+                <div class="col-lg-2 col-md-4"><a href="#" class="d-block megamenu-button-link dashbg-2"><i class="fa fa-clock-o"></i><strong>Demo 2</strong></a></div>
+                <div class="col-lg-2 col-md-4"><a href="#" class="d-block megamenu-button-link dashbg-3"><i class="fa fa-clock-o"></i><strong>Demo 3</strong></a></div>
+                <div class="col-lg-2 col-md-4"><a href="#" class="d-block megamenu-button-link dashbg-4"><i class="fa fa-clock-o"></i><strong>Demo 4</strong></a></div>
+                <div class="col-lg-2 col-md-4"><a href="#" class="d-block megamenu-button-link bg-danger"><i class="fa fa-clock-o"></i><strong>Demo 5</strong></a></div>
+                <div class="col-lg-2 col-md-4"><a href="#" class="d-block megamenu-button-link bg-info"><i class="fa fa-clock-o"></i><strong>Demo 6</strong></a></div>
+              </div>
+            </div>
+          </div>
+          <!-- Mega menu end-->
+
+          <!-- Languages dropdown -->
+          <div class="list-inline-item dropdown">
+            <a id="languages" rel="nofollow" data-target="#" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" class="nav-link language dropdown-toggle">
+              <img src="./usercss/img/English icon.jpg" alt="English" style="width: 24px; height: 24px; margin-right: 5px;">
+              <span class="d-none d-sm-inline-block">English</span>
+            </a>
+            <div aria-labelledby="languages" class="dropdown-menu">
+              <a rel="nofollow" href="#" class="dropdown-item" onclick="changeLanguage('Bangla')">
+                <img src="./usercss/img/Bangla icon.jpg" alt="Bangla" style="width: 24px; height: 24px; margin-right: 5px;">
+                <span>Bangla</span>
+              </a>
+            </div>
+          </div>
+          <!-- Script for change the language  -->
+          <!-- <script>
+            function changeLanguage(language) {
+              var languageElement = document.querySelector('#languages');
+              var languageImage = languageElement.querySelector('img');
+              var languageText = languageElement.querySelector('.d-none');
+
+              if (language === 'Bangla') {
+                languageImage.src = './img/Bangla icon.jpg';
+                languageText.innerText = 'Bangla';
+                //language === 'English';
+              } else if (language === 'English') {
+                languageImage.src = './img/English icon.jpg';
+                languageText.innerText = 'English';
+               // language === 'Bangla';
+              }
+              // Close the dropdown (if needed)
+              $(languageElement).dropdown('toggle');
+            }
+          </script> -->
+
+
+
+          <!-- Log out               -->
+          <div class="list-inline-item logout">
+            <a id="logout" href="login.html" class="nav-link" style="color: rgb(245, 78, 78); font-size: 20px">Logout
+              <i class="icon-logout"></i>
+            </a>
+          </div>
+        </div>
+    </nav>
+  </header>
+
+  <!-- Navbar, Header All Function are end here -->
